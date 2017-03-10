4c5369e75522fcd363e1e753073162ee9b88059b
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-10 12:00:42 -0500

    added folders/files

---
+++ models/cat.js
@@ -0,0 +1,26 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var cat = {
+  all: function(cb) {
+    orm.all("cats", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  // Create a new cat within the database
+  create: function(cols, vals, cb) {
+    orm.create("cats", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  // Update a cat within the database
+  update: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
